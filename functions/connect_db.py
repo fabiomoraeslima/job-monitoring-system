fcbeceef73d0133b50e612d357f518358b69746d
Fabio de Moraes Lima <103072860+fabiomoraeslima@users.noreply.github.com>
2024-06-21 10:18:00 -0300

    Update connect_db.py

--- functions/connect_db.py
+++ functions/connect_db.py
@@ -4,7 +4,7 @@ def fn_exec_querie(sql):
 
     # Conectar ao MySQL
     mydb = mysql.connector.connect(
-    host="192.168.0.226",
+    host="hostname",
     user="svc_report",
     password="report",
     database="suporte"
